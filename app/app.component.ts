4176f4cad2b62863a95cc010c03e7d2a4c9bf4d7
Oluwafemi Sule <oluwafemisule@outlook.com>
2016-05-16 11:59:27 +0100

    components, services, directives

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,15 @@
 import {Component} from '@angular/core';
+import { HeroesComponent }  from './heroes/heroes.component';
+
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+    <h1>{{ title }}</h1>
+    <toh-heroes></toh-heroes>
+    `,
+    directives: [HeroesComponent]
 })
-export class AppComponent { }
+export class AppComponent {
+    title = 'An Angular 2 force awakens';
+}
